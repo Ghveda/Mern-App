374c3e5567fdda6efea96d256b4a2ff603d629f5
ghveda <ghvedashvili1997@gmail.com>
2022-01-20 15:40:24 +0400

    Initialize git

---
+++ index.js
@@ -0,0 +1,22 @@
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+
+const { MONGODB } = require("./config");
+const resolvers = require("./graphql/resolvers");
+const typeDefs = require("./graphql/TypeDefs");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req }),
+});
+
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log("MongoDB Connected");
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
